24f26f2ddbd687409f29c7eabd47cc343bf2eef7
Kopsicle <giomargriffiths@gmail.com>
2026-04-07 14:26:33 -0500

    Update User.java
    Adding email, password_hash and salt fields with getters and setters.

--- CLEB2/src/main/java/com/cleb/model/User.java
+++ CLEB2/src/main/java/com/cleb/model/User.java
@@ -8,6 +8,9 @@ public abstract class User implements Serializable {
     private int userId;
     private String username;
     private String password;
+    private String email;
+    private String passwordHash;
+    private String salt;
     private Role role;
     
     
@@ -56,6 +59,32 @@ public abstract class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPasswordHash() {
+		return passwordHash;
+	}
+
+	public void setPasswordHash(String passwordHash) {
+		this.passwordHash = passwordHash;
+	}
+
+	public String getSalt() {
+		return salt;
+	}
+
+	public void setSalt(String salt) {
+		this.salt = salt;
+	}
 
 	public Role getRole() {
 		return role;
@@ -71,7 +100,7 @@ public abstract class User implements Serializable {
 
 	@Override
 	public String toString() {
-		return "User [userId=" + userId + ", username=" + username + ", password=" + password + ", role=" + role + "]";
+		return "User [userId=" + userId + ", username=" + username + ", email=" + email + ", password=" + password + ", role=" + role + "]";
 	}    
 
 }
